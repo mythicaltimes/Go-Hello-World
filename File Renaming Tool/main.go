ce81575c3c877a081107892e4b9baf49982113df
mythicaltimes <bmruze@gmail.com>
2019-01-28 19:32:45 -0500

    File Renaming Tool
    Finished part two of the File Renaming Tool

---
+++ File Renaming Tool/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"path/filepath"
+)
+
+func main() {
+
+	dir := "C:\\Go\\Projects\\Go-Hello-World\\File Renaming Tool\\samples"
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	count := 0
+	var toRename []string
+	for _, file := range files {
+		if file.IsDir() {
+		} else {
+			_, err := match(file.Name(), 4)
+			if err == nil{
+				count++
+				toRename = append(toRename, file.Name())
+			}
+		}
+	}
+	for _, origFilename := range toRename{
+		origPath := filepath.Join(dir, origFilename)
+		newFilename, err := match(origFilename, count)
+		if err != nil{
+			panic(err)
+		}
+		newPath := filepath.Join(dir, newFilename)
+		fmt.Printf("mv %s => %s", origPath, newPath)
+		err = os.Rename(origPath, newPath)
+		if err != nil{
+			panic(err)
+		}
+	}
+}
+
+// match returns the new file name or an error if the file name
+// didn't match our pattern
+func match(filename string, total int) (string, error) {
+	pieces := strings.Split(filename, ".")
+	ext := pieces[len(pieces)-1]
+	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
+	pieces = strings.Split(tmp, "_")
+	name := strings.Join(pieces[0:len(pieces)-1], "_")
+	number, err := strconv.Atoi(pieces[len(pieces)-1])
+	if err != nil {
+		return "", fmt.Errorf("%s didn't match our pattern", filename)
+	}
+	return fmt.Sprintf("%s - %d of %d.%s", strings.Title(name), number, total, ext), nil
+}
--- deck/card.go
+++ deck/card.go
@@ -1,18 +1,11 @@
 //go:generate stringer -type=Suit,Rank
-
 package deck
 
-import (
-	"testing"
-	"math/rand"
-	"sort"
-	"fmt"
-	"time"
-)
+import "fmt"
 
 type Suit uint8
 
-const(
+const (
 	Spade Suit = iota
 	Diamond
 	Club
@@ -20,27 +13,28 @@ const(
 	Joker // this is a special case
 )
 
-var suits = [...]Suit{Spade, Diamond, Club, Heart}
+var suits =[...]Suit{Spade, Diamond, Club, Heart}
 
 type Rank uint8
-const(
+
+const (
 	_ Rank = iota
 	Ace
 	Two
-	Three 
+	Three
 	Four
 	Five
 	Six
 	Seven
 	Eight
-	Nine	
-	Ten	
-	Jack	
+	Nine
+	Ten
+	Jack
 	Queen
 	King
 )
 
-const(
+const (
 	minRank = Ace
 	maxRank = King
 )
@@ -50,69 +44,21 @@ type Card struct {
 	Rank
 }
 
-
-func (c Card) String() string{
-	if c.Suit == Joker{
+func (c Card) String() string {
+	if c.Suit == Joker {
 		return c.Suit.String()
+
 	}
 	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
 }
 
-func New(opts ...func([]Card) []Card) []Card{
-	var cards []Card
-	for _, suit := range suits{
-		for rank :=minRank; rank <= maxRank; rank++{
-			cards = append(cards, Card{Suit: suit, Rank: rank})
-		}
-	}
-	for _, opt := range opts{
-		cards = opt(cards)
-	}
-	return cards
-}
 
-func DefaultSort(cards []Card) []Card{
-	sort.Slice(cards,Less(cards))
-	return cards
-}
-
-func Sort(less func(cards []Card) func(i, j int) bool) func([] Card) []Card{
-	return func(cards []Card) []Card {
-		sort.Slice(cards, less(cards))
-		return cards
+func New() []Card{
+var cards []Card
+for _, suit := range suits{
+	for rank := minRank; rank <= maxRank; rank++{
+		cards = append(cards, Card{Suit: suit, Rank: rank})
 	}
 }
-
-func Less(cards []Card) func (i, j int) bool{
-	return func(i, j int) bool{
-		return absRank(cards[i]) < absRank(cards[j])
-	}
+return cards
 }
-
-func absRank(c Card) int{
-	return int(c.Suit) *int(maxRank) + int(c.Rank)
-}
-
-
-func Shuffle(cards []Card) []Card{
-	ret := make([]Card, len(cards))
-	r := rand.New(rand.NewSource(time.Now().Unix()))
-	perm := r.Perm(len(cards))
-	for i, j := range r.Perm(len(cards)){
-		ret[i] = cards[j]
-	}
-	return ret
-}
-
-func Jokers(n int) func([]Card) []Card{
-	return func(cards []Card) []Card{
-		for i := 0; i <n; i++{
-			cards = append(cards, Card{
-			Rank: Rank(i),
-			Suit: Joker,
-			})
-		}
-		return cards
-	}
-}
-
